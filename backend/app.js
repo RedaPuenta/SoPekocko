eab27e356d14f724febdc360f250a1a32d341a2a
RedaPuenta <rida.hadj@hotmail.com>
2021-05-14 06:57:26 +0200

    Configuration --> protect vs XXS (helmet 11/15)

--- backend/app.js
+++ backend/app.js
@@ -3,26 +3,27 @@ const express = require("express")
 const app = express()
 
 const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
 const path = require("path")
+const helmet = require("helmet")
 
 const userRoute = require("./routes/users-R")
 const sauceRoute = require('./routes/sauces-R')
 
-mongoose.connect(process.env.DB_CONNECT,
-  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'))
+app.use(helmet())
+
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  next();
 });
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use("/api/auth", userRoute)
